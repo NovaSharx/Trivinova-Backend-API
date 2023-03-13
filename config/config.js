030bac31b4bfbda4c3c28e0e2ae26da958c6a238
Davin <davin-dj@hotmail.com>
2023-03-13 19:54:12 -0400

    Can now do sql cli migrations.

--- config/config.js
+++ config/config.js
@@ -1,31 +1,29 @@
 require('dotenv').config()
 
 module.exports = {
-  "development": {
-    // "url": "postgresql://trivinova-backend-api:jcTM58tr_mcdc813v5hM4Q@novasharx-cluster-5935.6wr.cockroachlabs.cloud:26257/trivinova?sslmode=verify-full",
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: "novasharx-cluster-5935.6wr.cockroachlabs.cloud",
-    port: 26257,
-    // ssl: "verify-full",
-    dialect: "postgres",
-    dialectOptions: {
-      encrypt: true
+    "development": {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialectOptions: {
+                ssl: true
+        },
+        dialect: "postgres",
+    },
+    "test": {
+        "username": "root",
+        "password": null,
+        "database": "database_test",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+    },
+    "production": {
+        "username": "root",
+        "password": null,
+        "database": "database_production",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
     }
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
 }
